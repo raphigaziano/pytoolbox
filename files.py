170819700d978e4f8a973639b5488efd6785bed9
raphigaziano <r.gaziano@gmail.com>
2013-07-12 15:42:17 +0200

    files.py - updated list_files function

--- files.py
+++ files.py
@@ -25,12 +25,13 @@ def list_files(startdir, recursive=False, abspathes=True, pattern=None):
     `abspathes`: Return absolute pathes. Defaults to True.
     `pattern`:   Unix glob pattern to filter files further
     """
-    for f in fnmatch.filter(os.listdir(startdir),
-                            pattern if pattern else '*'):
+    for f in os.listdir(startdir):
         path = os.path.join(startdir, f)
         if os.path.isfile(path):
-            if abspathes:
-                path = os.path.abspath(path)
+            if not abspathes:
+                path = os.path.relpath(path)
+            if pattern is not None and not fnmatch.fnmatch(path, pattern):
+                continue
             yield path
         elif recursive and os.path.isdir(path):
             for sub in list_files(path, recursive, abspathes):
